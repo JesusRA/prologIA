bd563e911bba299fe4839b25d1849f222ca70967
JESUS <alu0100904932@cc1333.etsii.ull.es>
2017-11-13 15:50:22 +0000

    aún más ejemplos

--- fichero3.pl
+++ fichero3.pl
@@ -1,2 +1,10 @@
-member(X,[X,T]).
-member(X,[X,T]):-member(X,T).
+len([],0).
+len([_|L],N):-
+  len(L,X),
+  N is X + 1.
+
+acclen([], Acc, Length):-
+  Length = Acc.
+acclen([_|L], OldAcc, Length):-
+  NewAcc is OldAcc + 1,
+  acclen(L, NewAcc, Length).
