8b0fd6b46b6508f010b16a3e2f72fe52281c9926
JesusRA <jesus.ra.1993@gmail.com>
2017-11-25 22:24:06 +0000

    Resuelto puzzle

--- puzzle.pl
+++ puzzle.pl
@@ -1,11 +1,15 @@
-% en mayúsculas porque son variables
-[Atico,Piso4,Piso3,Piso2,Bajo]
-% en minúsculas porque son átomos
-[juan,antonio,ana,maria,luis]
-% si vive más alto está más a la izquierda en nuestra lista
-% definir higher y not_adjacent
-higher().
-not_adjacent().
+higher(X,Y,[X|T]):-
+  member(Y,T).
+higher(X,Y,[H|T]):-
+  higher(X,Y,T).
+
+adjacent(X,Y,[X,Y|_]).
+adjacent(X,Y,[_|L]):-adjacent(X,Y,L).
+
+not_adjacent(X,Y,L):-
+  not(adjacent(X,Y,L)),
+  not(adjacent(Y,X,L)).
+
 puzzle(L):-
   permutation(L,[juan,antonio,ana,maria,luis]),
   L = [Atico,Piso4,Piso3,Piso2,Bajo],
