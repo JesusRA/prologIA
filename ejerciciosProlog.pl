fde03a6c1aefca319f9772ba95df24020364ba34
JesusRA <jesus.ra.1993@gmail.com>
2017-11-25 22:28:28 +0000

    Unido todo en el mismo archivo para entrega

--- ejerciciosProlog.pl
+++ ejerciciosProlog.pl
@@ -125,3 +125,28 @@ modulo(M,N,M):-
 modulo(M,N,X):-
   M >= N,
   X is mod(M,N).
+
+% Puzzle
+
+higher(X,Y,[X|T]):-
+  member(Y,T).
+higher(X,Y,[H|T]):-
+  higher(X,Y,T).
+
+adjacent(X,Y,[X,Y|_]).
+adjacent(X,Y,[_|L]):-adjacent(X,Y,L).
+
+not_adjacent(X,Y,L):-
+  not(adjacent(X,Y,L)),
+  not(adjacent(Y,X,L)).
+
+puzzle(L):-
+  permutation(L,[juan,antonio,ana,maria,luis]),
+  L = [Atico,Piso4,Piso3,Piso2,Bajo],
+  juan \== Atico,
+  antonio \== Bajo,
+  ana \== Atico,
+  ana \== Bajo,
+  higher(luis,antonio,L),
+  not_adjacent(maria,ana,L),
+  not_adjacent(ana,antonio,L).
